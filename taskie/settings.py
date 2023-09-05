a2762cde7354e296f6896a878422e16fcf3f3c04
Cristobal <cinzunza@apside.cl>
2023-09-05 01:50:49 -0300

    Formulario de login y welcome page

--- taskie/settings.py
+++ taskie/settings.py
@@ -111,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'America/Santiago'
 
@@ -134,3 +134,5 @@ STATICSFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+X_FRAME_OPTIONS = 'SAMEORIGIN'
--- user_manager/urls.py
+++ user_manager/urls.py
@@ -3,5 +3,9 @@ from . import views
 
 app_name = 'user_manager'
 urlpatterns = [
-    path("", views.index, name="index"),
+    # path("", views.index, name="index"),
+    path("", views.welcome, name="welcome"),
+    path("signin/", views.signin, name="signin"),
+    path("signup/", views.signup, name="signup"),
+    path("signout/", views.signout, name="signout"),
 ]
